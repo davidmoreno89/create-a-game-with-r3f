0643b203d550b97f0fc240a1cf5a0f4f6b394b03
David Moreno <moreno@rushstreetinteractive.com>
2024-05-05 16:30:27 -0500

    Adding r3f game

---
+++ src/Experience.jsx
@@ -0,0 +1,20 @@
+/* import { OrbitControls } from '@react-three/drei' */
+import { Physics } from '@react-three/rapier'
+import Lights from './Lights.jsx'
+import { Level } from '/Level.jsx'
+import Player from './Player.jsx'
+import useGame from './stores/useGame.jsx'
+
+export default function Experience() {
+    const blocksCount = useGame((state) => state.blocksCount)
+    const blocksSeed = useGame(state => state.blocksSeed)
+    return <>
+        {/* <OrbitControls makeDefault /> */}
+        <color args={['#bdedfc']} attach="background" />
+        <Physics>
+            <Lights />
+            <Level count={blocksCount} seed={blocksSeed} />
+            <Player />
+        </Physics>
+    </>
+}
